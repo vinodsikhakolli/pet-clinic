599f37c2ed5e5e44beb2a788ea3d25eda9269ce8
Vinod Kumar Sikhakolli <vinodkumar.sikhakolli@ericsson.com>
2019-06-07 16:19:03 +0530

    Adding Pojos

---
+++ src/main/java/com/vinod/learning/petclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.vinod.learning.petclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/com/vinod/learning/petclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.vinod.learning.petclinic.model;
+
+public class Vet extends Person {
+}
